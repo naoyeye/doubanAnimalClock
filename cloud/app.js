21c46a13fb26ec1b015e41d958f64fb805381737
J.Y Han <ihanjiyun@gmail.com>
2015-07-01 02:20:33 +0800

    fix schedule setting

--- cloud/app.js
+++ cloud/app.js
@@ -2,7 +2,7 @@
 * @Author: Jiyun
 * @Date:   2015-06-25 03:35:03
 * @Last Modified by:   Jiyun
-* @Last Modified time: 2015-06-30 11:22:45
+* @Last Modified time: 2015-07-01 02:20:08
 */
 
 // jshint ignore:start
@@ -98,7 +98,7 @@ app.get('/', function (req, res) {
 
             // 定义自动定时任务的规则
             var rule = new schedule.RecurrenceRule();
-            rule.minute = [0]; // 会有延迟
+            rule.minute = [0, 60]; // 会有延迟
 
             /* just for testing */
             // var text = generateText();
